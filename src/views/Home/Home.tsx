54576d484d7e3763368c95fc701cee852473601d
Jia Jian Goi <jiajian@cakedefi.com>
2022-09-20 03:01:31 +0800

    feat(decrypt): added decryption using sjcl

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,17 +1,28 @@
-import { Col, Row, Typography, Form, Button, Input } from "antd";
-import * as BitGo from "bitgo";
-import React from "react";
+import { Col, Row, Typography, Form, Button, Input, Card } from "antd";
+import React, { useState } from "react";
+import * as SJCL from "sjcl";
 import { RoundedCard } from "../../components/Shared/RoundedCard";
 import { AppLayoutWrapper } from "../../components/UI/AppLayoutWrapper";
 
 const { Title } = Typography;
 const { TextArea } = Input;
 
+interface DecryptFormFields {
+  privateKey?: string;
+  password?: string;
+}
+
 const Home: React.FunctionComponent = () => {
   const [form] = Form.useForm();
-  console.log(form);
-  const bitgo = new BitGo.BitGo({ env: "test" });
-  console.log(bitgo);
+  const [privateKey, setPrivateKey] = useState("");
+
+  const onFinish = (values: DecryptFormFields): void => {
+    if (values?.privateKey && values?.password) {
+      console.log(JSON.parse(values.privateKey));
+      const result = SJCL.decrypt(values?.password, values.privateKey);
+      setPrivateKey(result);
+    }
+  };
 
   return (
     <>
@@ -37,22 +48,37 @@ const Home: React.FunctionComponent = () => {
                 </Title>
 
                 <Row>
-                  <Col>
+                  <Col span={12} style={{ paddingRight: 16 }}>
                     {" "}
-                    <Form form={form} layout="vertical">
-                      <Form.Item label="Encrypted Private Key">
+                    <Form form={form} layout="vertical" onFinish={onFinish}>
+                      <Form.Item
+                        label="Encrypted Private Key"
+                        name="privateKey"
+                        rules={[{ required: true, message: "This field is required" }]}
+                      >
                         <TextArea
                           rows={6}
                           placeholder='Should look like {"iv":"XXXXXXXX==","v":1,"iter":10000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"abcdefGH=","ct":"XXXXX"}'
                         />
                       </Form.Item>
-                      <Form.Item label="Encryption Password">
+                      <Form.Item
+                        label="Encryption Password"
+                        name="password"
+                        rules={[{ required: true, message: "This field is required" }]}
+                      >
                         <Input.Password placeholder="Your password used to encrypt the above private key." />
                       </Form.Item>
                       <Form.Item>
-                        <Button type="primary">Submit</Button>
+                        <Button type="primary" htmlType="submit" size="large">
+                          Decrypt
+                        </Button>
                       </Form.Item>
                     </Form>
+                  </Col>
+                  <Col span={12}>
+                    <Card title="Decrypted Private Key" bordered={false} style={{ width: 300 }}>
+                      <p>{privateKey}</p>
+                    </Card>
                   </Col>
                 </Row>
               </Col>
